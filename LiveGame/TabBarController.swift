88d2bd63de1793c691791f3b87cb62c9c7590d06
nguyen.dinh.thach <nguyendinhthach@sun-asterisk.com>
2020-09-15 16:42:17 +0700

    - first commit

--- LiveGame/TabBarController.swift
+++ LiveGame/TabBarController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
 
 }
 
